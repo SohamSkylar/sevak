a7ecc1e6d215bdcb3f49ae8db648c7579e6d6c5b
SohamSkylar <sohamsarkar2001@gmail.com>
2023-01-10 18:20:46 +0530

    ScrapTemplate Initiated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ScrapTemplate from './components/ScrapTemplate';
 
 function App() {
   return (
     <>
+      <ScrapTemplate/>
     </>
   );
 }
